42337477cbcb353081dc4b18194e740da794939b
Manuel Neto <manuel.neto@ccc.ufcg.edu.br>
2017-04-01 20:59:26 -0300

    refatoramento da view

--- src/app.js
+++ src/app.js
@@ -1,8 +1,3 @@
-angular.module('app', ['ui.router', 'ui.router.stateHelper']);
-
-//Require all files here
-require('./config/routes.config.js');
-require('./components/maquinaTuring/maquinaTuring.controller.js');
-
-//Sass
-require('./theme-sass/theme.scss');
+angular.module("turingMachine",[]).controller("appController", function($scope){
+   $scope.machineInput;
+});
--- src/components/maquinaTuring/maquinaTuring.controller.js
+++
@@ -1,3 +0,0 @@
-angular.module('app').controller('MaquinaTuringController', function ($scope, $state) {
-
-});
